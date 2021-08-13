fc2c3681cccc7101ce0e9b47b8050e82101d7b57
Ming Wang <jasonming1998@gmail.com>
2021-08-13 15:44:27 +0800

    优化

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,6 +1,6 @@
 /*
- * @Descripttion: 
- * @version: 
+ * @Descripttion:
+ * @version:
  * @Author: WangMing
  * @Date: 2021-08-04 15:42:36
  * @LastEditors: Please set LastEditors
@@ -11,7 +11,7 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -10,12 +10,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FileserverModule } from './fileserver/fileserver.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
 
 @Module({
-  imports: [FileserverModule],
+  imports: [TypeOrmModule.forRoot(), FileserverModule],
+
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {
-
+  constructor(private readonly connection: Connection) {}
 }

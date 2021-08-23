a78db292e14b74c8d9ef3ae470629d445dd4969b
wangming <jasonming1998@gmail.com>
2021-08-23 16:56:36 +0800

    优化

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,18 @@
-cd api
+echo "构建API"
+cd ./api
 export version="v0.2"
-docker build -t wangming19980219/mos-fe:$version .
-docker pull wangming19980219/mos-fe:$version
+docker build -t wangming19980219/mos-api .
+# docker pull wangming19980219/mos-fe:$version
 
 
-echo "开启api"
-docker run -it -v /var/www/html:/var/www/html -p 6657:3000 wangming19980219/mos-api:v1 bin/bash
+echo "开启API"
+docker run -it -v /var/www/html:/var/www/html -p 6657:3000 wangming19980219/mos-api bin/bash
+
+
+cd ../fe
+echo "构建FE"
+docker build -t wangming19980219/mos-fe .
+
+echo "开启FE"
+docker run -itd wangming19980219/mos-fe
+
